1be3f278d990a79c5f5a84b53fbfe41756bc7ca0
cullenn <cullenmclaren@gmail.com>
2020-01-02 15:22:24 -0500

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+const channels = [
+  "voted-suggestions",
+  "classes",
+  "items",
+  "pvp",
+  "clans",
+  "misc"
+];
+
+bot.login(TOKEN);
+
+bot.on("ready", () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on("message", msg => {
+  if (channels.includes(msg.channel.name)) {
+    msg.react("662386640185983027");
+    msg.react("662386640253091850");
+  }
+});
